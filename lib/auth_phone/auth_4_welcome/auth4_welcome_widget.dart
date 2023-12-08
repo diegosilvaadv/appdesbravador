31a0e646ce32fcbabefec612374a47cf9ea5d335
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-08 16:08:42 +0000

    Updating to latest FlutterFlow output.

--- lib/auth_phone/auth_4_welcome/auth4_welcome_widget.dart
+++ lib/auth_phone/auth_4_welcome/auth4_welcome_widget.dart
@@ -1,3 +1,4 @@
+import '/auth/firebase_auth/auth_util.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
@@ -251,8 +252,15 @@ class _Auth4WelcomeWidgetState extends State<Auth4WelcomeWidget> {
                       padding: EdgeInsetsDirectional.fromSTEB(
                           20.0, 12.0, 20.0, 12.0),
                       child: FFButtonWidget(
-                        onPressed: () {
-                          print('Button pressed ...');
+                        onPressed: () async {
+                          GoRouter.of(context).prepareAuthEvent();
+                          final user =
+                              await authManager.signInWithGoogle(context);
+                          if (user == null) {
+                            return;
+                          }
+
+                          context.goNamedAuth('HomePage', context.mounted);
                         },
                         text: 'Entar com  Google',
                         icon: FaIcon(
